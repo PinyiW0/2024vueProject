7bd3e850679282f670010733e06abdfb0321c765
Pinyi Wu <wpypy8@gmail.com>
2024-02-01 22:37:34 +0800

    fix: 🍮 修正productModal v-model

--- src/components/ProductModal.vue
+++ src/components/ProductModal.vue
@@ -16,7 +16,7 @@
                         <div class="mb-3">
                           <label for="imageUrl" class="form-label">請輸入主要產品圖網址</label>
                           <input type="text" class="form-control"
-                                 placeholder="請輸入圖片連結" id="imageUrl" v-model="tempProduct.imageUrl">
+                                 placeholder="請輸入圖片連結" id="imageUrl" v-model="curProduct.imageUrl">
                         </div>
                         <img class="img-fluid" :src="tempProduct.imageUrl" alt="">
                       </div>
@@ -27,16 +27,16 @@
                           <div v-for="(img, key) in tempProduct.imagesUrl" :key="key + 123">
                             <img :src="img" alt="" class="img-fluid">
                             <label for="imageUrl" class="form-label mt-3">請輸入圖片網址</label>
-                            <input type="text" class="form-control mb-3" v-model="tempProduct.imagesUrl[key]"> <!-- 綁索引位置才能在替換圖片網址時換成相對應位置的圖 -->
+                            <input type="text" class="form-control mb-3" v-model="curProduct.imagesUrl[key]"> <!-- 綁索引位置才能在替換圖片網址時換成相對應位置的圖 -->
                           </div>
                           <button class="btn btn-outline-primary w-100 mb-3"
                             v-if = "tempProduct.imagesUrl.length === 0 ||
                               tempProduct.imagesUrl[tempProduct.imagesUrl.length - 1]
-                              " @click.prevent="tempProduct.imagesUrl.push('')">
+                              " @click.prevent="curProduct.imagesUrl.push('')">
                             新增圖片
                           </button>
                           <button class="btn btn-outline-danger mb-3 d-block w-100"
-                            v-else @click.prevent="tempProduct.imagesUrl.pop()">
+                            v-else @click.prevent="curProduct.imagesUrl.pop()">
                             刪除圖片
                           </button>
                         </div>
@@ -45,30 +45,30 @@
                     <div class="col-sm-8">
                       <div class="mb-3">
                         <label for="title" class="form-label">標題</label>
-                        <input id="title" type="text" class="form-control" placeholder="請輸入標題" v-model.trim="tempProduct.title">
+                        <input id="title" type="text" class="form-control" placeholder="請輸入標題" v-model.trim="curProduct.title">
                       </div>
 
                       <div class="row">
                         <div class="mb-3 col-md-6">
                           <label for="category" class="form-label">分類</label>
                           <input id="category" type="text" class="form-control"
-                                 placeholder="請輸入分類" v-model="tempProduct.category">
+                                 placeholder="請輸入分類" v-model="curProduct.category">
                         </div>
                         <div class="mb-3 col-md-6">
                           <label for="price" class="form-label">單位</label>
-                          <input id="unit" type="text" class="form-control" placeholder="請輸入單位" v-model="tempProduct.unit">
+                          <input id="unit" type="text" class="form-control" placeholder="請輸入單位" v-model="curProduct.unit">
                         </div>
                       </div>
 
                       <div class="row">
                         <div class="mb-3 col-md-6">
                           <label for="origin_price" class="form-label">原價</label>
-                          <input id="origin_price" type="number" min="0" class="form-control" placeholder="請輸入原價"  v-model="tempProduct.origin_price">
+                          <input id="origin_price" type="number" min="0" class="form-control" placeholder="請輸入原價"  v-model="curProduct.origin_price">
                         </div>
                         <div class="mb-3 col-md-6">
                           <label for="price" class="form-label">售價</label>
                           <input id="price" type="number" min="0" class="form-control"
-                                 placeholder="請輸入售價" v-model="tempProduct.price">
+                                 placeholder="請輸入售價" v-model="curProduct.price">
                         </div>
                       </div>
                       <hr>
@@ -76,19 +76,19 @@
                       <div class="mb-3">
                         <label for="description" class="form-label">產品描述</label>
                         <textarea id="description" type="text" class="form-control"
-                                  placeholder="請輸入產品描述" v-model="tempProduct.description">
+                                  placeholder="請輸入產品描述" v-model="curProduct.description">
                     </textarea>
                       </div>
                       <div class="mb-3">
                         <label for="content" class="form-label">說明內容</label>
                         <textarea id="description" type="text" class="form-control"
-                                  placeholder="請輸入說明內容" v-model="tempProduct.content">
+                                  placeholder="請輸入說明內容" v-model="curProduct.content">
                     </textarea>
                       </div>
                       <div class="mb-3">
                         <div class="form-check">
                           <input id="is_enabled" class="form-check-input" type="checkbox"
-                                 :true-value="1" :false-value="0"  v-model="tempProduct.is_enabled">
+                                 :true-value="1" :false-value="0"  v-model="curProduct.is_enabled">
                           <label class="form-check-label" for="is_enabled">是否啟用</label>
                         </div>
                       </div>
